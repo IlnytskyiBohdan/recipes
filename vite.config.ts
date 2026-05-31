930ad51d7b8ce7010357ffd422bc8ac76c3bae07
IlnytskyiBohdan <neklz1@icloud.com>
2026-05-31 21:39:28 +0300

    split bundle, fix html validator

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/recipes/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "vendor-react": ["react", "react-dom", "react-router-dom"],
+          "vendor-mui": ["@mui/material", "@mui/icons-material", "@mui/system"],
+          "vendor-query": ["@tanstack/react-query"],
+          "vendor-misc": ["zustand", "lodash"],
+        },
+      },
+    },
+  },
 });
