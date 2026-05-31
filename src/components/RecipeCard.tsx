584e73720cf64f3978dc372c7e0ea5beeba4f3a2
IlnytskyiBohdan <neklz1@icloud.com>
2026-05-31 21:46:26 +0300

    remove lazy loading

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -32,9 +32,6 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ meal }) => {
           component="img"
           src={meal.strMealThumb}
           alt={meal.strMeal}
-          loading="lazy"
-          width={700}
-          height={527}
           sx={{
             width: "100%",
             height: { xs: 140, sm: 190 },
